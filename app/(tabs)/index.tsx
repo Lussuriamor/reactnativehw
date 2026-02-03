dbe530e7d9f65fffac84642c8620a0723278547a
Lussuriamor <err3735@gmail.com>
2026-02-03 22:49:45 +0600

    rnhw1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Button title="Elaman" onPress={() => {}} />
+      <Text style={styles.name}>Elaman</Text>
+      <Text style={styles.course}>React Native</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>CLICK</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,5 +18,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  course: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
